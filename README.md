d6701618dbb669f71a0e0a493b94a880d0defe0c
Impsaccrain <149641770+Impsaccrain@users.noreply.github.com>
2024-11-05 23:49:14 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -27,7 +27,8 @@ npm update -g c6-compiler
 This language was made with the goal of being just as fast as languages like C or C++, while maintaining readability
 and easy usage. It provides a lot of extra functions and less boilerplate, as will be discussed in another section of the README.
 
-# The C6 Compiler
+# C6 Documentation (v1.2.0)
+## Include Statements
 The C6 language is a "superset" of the C++ programming language. It contains features such as automatic inclusion of
 `#include` statements for standard libraries, meaning you no longer have to say things like `#include <iostream>`,
 `#include <random>`, `#include <string>`, etc. You now only need to use it when importing from a local file. These
@@ -37,19 +38,94 @@ you can still write these local files in C6, just make sure to compile them and 
 To demonstrate the removal of `#include` statements, here is an example of a hello world program in C6:
 ```cpp
 int main() {
-  std::cout << "Hello, world!" << std::endl;
+  c6::println("Hello, world!"); // c6::println is a custom function, see more under the C6 namespace.
   return 0;
 };
 ```
 Compiling this script will automatically make it include `<iostream>`, as that is what is required here.
-But here is a better example of a hello world program in C6:
 
+## Templates
+Templates in the C6 language have been heavily improved from C++ templates. A standard C++ template might look like this:
 ```cpp
-int main() {
-  c6::println("Hello, world!");
-  return 0;
+template<typename T>
+class MyTemplate { /* code */ };
+```
+
+While you can still use this syntax in C6 if you like, C6 now offers a better way to do this:
+```cpp
+class MyTemplate<T> { /* code */ };
+```
+
+This will have the same effect as the first code snippet. You can even do this with other types of parameters.
+Here is an example in C++:
+```cpp
+template<typename T, int someNumber, typename K = int>
+class MyTemplate { /* code */ };
+```
+
+And here is how you can achieve this in C6:
+```cpp
+class MyTemplate<T, int someNumber, K = int> { /* code */ };
+```
+
+You can even do this with C++20's `requires` clauses. Here is a C++ example:
+```cpp
+template<typename T = int>
+requires MyConcept<T>
+class MyTemplate { /* code */ };
+```
+
+And here is how you can do this in C6:
+```cpp
+class MyTemplate<T requires MyConcept<T> = int> { /* code */ };
+```
+
+## Lambda Functions
+Lambda functions in the C6 language have also been improved. This is an example of a C++ lambda:
+```cpp
+auto lambda = [](int someParam) {
+    return someParam + 10;
 };
 ```
+
+And this is a C6 lambda function:
+```cpp
+auto lambda = fn(int someParam) {
+    return someParam + 10;
+};
+```
+
+Here is how you can replicate the use of more complex lambdas from C++:
+```cpp
+auto lambda = [&x, &y]() {
+    x += y; // Modifies the original x
+};
+```
+
+C6:
+```cpp
+auto lambda = fn() {
+    x += y; // Modifies the original x
+};
+```
+
+As shown here, you can use variables from outside the scope of the lambda function. All variables from
+outside the lambda function will be passed by reference (essentially `[&]` in C++). If you wish to pass
+a variable by value, just make it a parameter.
+Example:
+```cpp
+int main() {
+    int x = 0;
+    auto increment = fn(int &x) {
+        x++;
+    };
+    increment(x); // x must be passed as an argument
+    return 0;
+};
+```
+
+## The C6 Namespace
+
 Just as `std` is C++'s standard namespace, `c6` is C6's standard namespace. C6 still supports everything that is
 in C++'s `std`, but its `c6` namespace makes a lot of operations easier. This language is aimed to make C++ easier.
 
@@ -58,30 +134,33 @@ Just like C++'s `std`, you can also put `using namespace c6;` at the top of your
 `c6` and `std` at the top of your file using `using namespace`. This is because a few `c6` functions have the same name as
 `std` functions, which could cause confusion.
 
-# Function Documentation (v1.1.0)
+Here is the list of functions:
 
-**`int c6::randint(int min, int max)`**
+**`float c6::randfloat(const float min, const float max)`**
+Generates a random float between the min and the max values then returns it.
+
+**`int c6::randint(const int min, const int max)`**
 Generates a random integer between the min and the max values then returns it.
 
-**`void c6::write_file(std::string path, std::string contents, bool overwrite = true)`**
+**`void c6::write_file(const std::string& path, const std::string& contents, const bool overwrite = true)`**
 Writes the contents to the file at the provided path. Overwrite is optional and determines whether to overwrite
 the file or append to it. If there is no file at the provided path it will raise a `std::runtime_error`.
 
-**`std::string c6::read_file(std::string path)`**
+**`std::string c6::read_file(const std::string& path)`**
 Gets the contents of the file at the provided path and returns them. If there is no file at the provided
 path it will raise a `std::runtime_error`.
 
-**`void c6::println(T text)`**
+**`void c6::println(const T... text)`**
 Prints the provided text to the console with an automatic newline. The text can be a variety of types, including
 `std::string`, `int`, `long`, `double`, `float`, and other primitive types.
 
-**`std::string c6::input(T text)`**
+**`std::string c6::input(const T& text)`**
 Prints the provided text to the console and waits for user input. Once it recieves user input, it will return it as a `std::string`.
 
-**`int c6::to_int(std::string value)`**
+**`int c6::to_int(const std::string& value)`**
 Attempts to convert the `std::string` value to an int then return it. If it fails, it will throw a `std::runtime_error`.
 
-**`std::string c6::to_string(T value)`**
+**`std::string c6::to_string(const T value)`**
 Will convert any number-like primitive type to a string. This includes the following:
 - `int`
 - `long`
@@ -92,11 +171,26 @@ Will convert any number-like primitive type to a string. This includes the follo
 - `float`
 - `double`
 - `long double`
+- `bool`
 
 # Changelog
 
-v1.1.0 - Added the C6 help menu, the documentation, and released it to the public.
+v1.2.0
+- Made templates easier.
+- Made lambda functions easier.
+- Improved README.md.
+- Made `c6::println` accept any number of arguments.
+- Made `c6::to_string` support `bool` types.
+- Added `c6::randfloat`.
+- Improved automatic header placement.
 
-v1.0.1 - Fixed a critical bug in compilation.
+v1.1.0
+- Added C6 help menu.
+- Added documentation.
+- Public release.
 
-v1.0.0 - Initial creation.
+v1.0.1
+- Fixed a critical compiler bug.
+
+v1.0.0
+- Initial creation and the C6 namespace.
--- index.js
+++ index.js
@@ -32,12 +32,12 @@ const headerMap = {
     '<atomic>': ['atomic', 'atomic_flag', 'atomic_int'],
     '<functional>': ['function', 'bind', 'packaged_task'],
     '<memory>': ['unique_ptr', 'shared_ptr', 'weak_ptr', 'make_unique', 'make_shared', 'allocator'],
-    '<type_traits>': ['is_same', 'enable_if', 'decay', 'is_base_of', 'remove_pointer'],
+    '<type_traits>': ['is_same', 'enable_if', 'decay', 'is_base_of', 'remove_pointer', 'is_same_v'],
     '<optional>': ['optional'],
     '<variant>': ['variant'],
     '<tuple>': ['tuple', 'get', 'make_tuple'],
     '<any>': ['any', 'any_cast'],
-    '<exception>': ['exception', 'runtime_error', 'logic_error', 'bad_alloc'],
+    '<exception>': ['exception', 'bad_alloc', 'bad_cast', 'bad_typeid'],
     '<limits>': ['numeric_limits'],
     '<iterator>': ['iterator', 'reverse_iterator', 'ostream_iterator'],
     '<regex>': ['regex', 'smatch', 'sregex_iterator', 'regex_search', 'regex_replace'],
@@ -45,7 +45,7 @@ const headerMap = {
     '<filesystem>': ['path', 'file_status', 'directory_iterator', 'recursive_directory_iterator'],
     '<iomanip>': ['setprecision', 'fixed', 'scientific', 'showpoint', 'noshowpoint', 'setw', 'setfill', 'left', 'right', 'internal', 'hex', 'dec', 'oct', 'showbase', 'noshowbase', 'showpos', 'noshowpos'],
     '<random>': ['minstd_rand', 'mt19937', 'mt19937_64', 'ranlux24_base', 'ranlux48_base', 'default_random_engine', 'random_device', 'uniform_int_distribution', 'uniform_real_distribution', 'bernoulli_distribution', 'binomial_distribution', 'negative_binomial_distribution', 'poisson_distribution', 'normal_distribution', 'lognormal_distribution', 'exponential_distribution', 'weibull_distribution', 'gamma_distribution', 'chi_squared_distribution', 'cauchy_distribution', 'student_t_distribution', 'seed_seq', 'shuffle', 'generate'],
-    '<stdexcept>': ['invalid_argument', 'out_of_range', 'runtime_error'],
+    '<stdexcept>': ['invalid_argument', 'out_of_range', 'runtime_error', 'length_error', 'overflow_error', 'logic_error', 'domain_error', 'range_error', 'underflow_error'],
     '<string>': ['string', 'basic_string', 'wstring', 'u16string', 'u32string', 'getline', 'string_view', 'to_string', 'stoi', 'stol', 'stoll', 'stof', 'stod', 'stold', 'strchr', 'strrchr', 'strstr', 'compare', 'swap']
 };
 
@@ -144,7 +144,8 @@ const customFunctionMap = {
                 name: 'min',
                 type: {
                     namespace: null,
-                    name: 'int'
+                    name: 'int',
+                    const: true
                 },
                 optional: false
             },
@@ -152,7 +153,8 @@ const customFunctionMap = {
                 name: 'max',
                 type: {
                     namespace: null,
-                    name: 'int'
+                    name: 'int',
+                    const: true
                 },
                 optional: false
             }
@@ -164,9 +166,42 @@ const customFunctionMap = {
             return dis(gen);
         `
     },
+    randfloat: {
+        returnType: {
+            namespace: null,
+            name: 'float'
+        },
+        params: [
+            {
+                name: 'min',
+                type: {
+                    namespace: null,
+                    name: 'float',
+                    const: true
+                },
+                optional: false
+            },
+            {
+                name: 'max',
+                type: {
+                    namespace: null,
+                    name: 'float',
+                    const: true
+                },
+                optional: false
+            }
+        ],
+        code: `
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<float> distr(min, max);
+            return distr(gen);
+        `
+    },
     println: {
         template: {
-            typename: 'T'
+            custom: true,
+            value: 'typename... Args'
         },
         returnType: {
             namespace: null,
@@ -177,13 +212,14 @@ const customFunctionMap = {
                 name: 'text',
                 type: {
                     namespace: null,
-                    name: 'T'
+                    name: 'Args...',
+                    const: true
                 },
                 optional: false
             }
         ],
         code: `
-            std::cout << text << std::endl;
+            (std::cout << ... << text) << std::endl;
         `
     },
     input: {
@@ -254,27 +290,30 @@ const customFunctionMap = {
                 type: {
                     namespace: null,
                     name: 'T',
+                    const: true
                 },
                 optional: false
             }
         ],
         code: `
+            if constexpr (std::is_same_v<T, bool>) {
+                if (value) {
+                    return "true";
+                } else {
+                    return "false";
+                };
+            };
             return std::to_string(value);
         `
     }
 };
 
 function compile(filePath) {
-    function getRequiredHeaders(code) {
-        code = code.replace(/\/\/.*?$|\/\*.*?\*\//g, '');
-        code = code.toLowerCase();
-
-        const identifiers = code.match(/\b[a-zA-Z_][a-zA-Z\d_]*\b/g);
-
+    function getRequiredHeaders(tokens) {
         const requiredHeaders = new Set();
 
         for (let header in headerMap) {
-            if (identifiers.some(i => headerMap[header].includes(i))) {
+            if (headerMap[header].some(h => searchFunction(tokens, h, 'std'))) {
                 requiredHeaders.add(header);
             };
         };
@@ -297,11 +336,164 @@ function compile(filePath) {
         const data = customFunctionMap[name];
         let templateStr = '';
         if (data.template) {
-            templateStr = 'template<typename ' + data.template.typename + '>\n'
+            if (data.template.custom) {
+                templateStr = `template<${data.template.value}>\n`;
+            } else {
+                templateStr = 'template<typename ' + data.template.typename + '>\n';
+            };
         };
         const params = data.params.map(i => `${getTypeName(i.type)} ${i.name}${i.optional !== false ? ' = ' + i.optional : ''}`).join(', ');
         const funcName = `${getTypeName(data.returnType)} ${name}(${params}) {`;
         return templateStr + funcName + '\n' + data.code + '\n};';
+    };
+
+    function getTokens(code) {
+        const tokens = [];
+        let currentType = 'normal';
+        let value = '';
+        let stringEscaped = false;
+        let stringType = '"';
+        function resetTokenType(letter) {
+            if (/^[a-zA-Z\d_]$/.test(letter)) {
+                currentType = 'normal';
+            } else {
+                currentType = 'symbol-normal';
+            };
+        };
+        for (let i = 0; i < code.length; i++) {
+            const letter = code[i];
+            switch (currentType) {
+                case 'symbol-normal':
+                case 'normal': {
+                    value += letter;
+                    if (code[i + 1] == '"' || code[i + 1] == '\'') {
+                        if (value) tokens.push(value);
+                        value = '';
+                        stringEscaped = false;
+                        currentType = 'string';
+                        stringType = code[i + 1];
+                    } else if (code[i + 1] == '/' && code[i + 2] == '/') {
+                        currentType = 'oneline-comment';
+                    } else if (code[i + 1] == '/' && code[i + 2] == '*') {
+                        currentType = 'multiline-comment';
+                    } else if (currentType == 'normal' && !/^[a-zA-Z\d_]$/.test(code[i + 1])) {
+                        if (value) tokens.push(value);
+                        value = '';
+                        currentType = 'symbol-normal';
+                    } else if (currentType == 'symbol-normal' && /^[a-zA-Z\d_]$/.test(code[i + 1])) {
+                        if (value) tokens.push(value);
+                        value = '';
+                        currentType = 'normal';
+                    };
+                    break;
+                };
+                case 'string': {
+                    value += letter;
+                    if (letter == stringType && !stringEscaped && value.length > 1) {
+                        tokens.push(value);
+                        value = '';
+                        resetTokenType(code[i + 1]);
+                    } else if (letter == '\\' && !stringEscaped) {
+                        stringEscaped = true;
+                    } else {
+                        stringEscaped = false;
+                    };
+                    break;
+                };
+                case 'oneline-comment': {
+                    if (code[i + 1] == '\n') {
+                        currentType = 'symbol-normal';
+                    };
+                    break;
+                };
+                case 'multiline-comment': {
+                    if (letter == '/' && code[i - 1] == '*') {
+                        resetTokenType(code[i + 1]);
+                    };
+                    break;
+                };
+            };
+        };
+        if (value) tokens.push(value);
+        const tokensNoWhitespace = tokens.reduce((acc, token) => {
+            const noWhitespace = token.replace(/\s/g, '');
+            if (noWhitespace) {
+                return [...acc, noWhitespace];
+            } else {
+                return acc;
+            };
+        }, []);
+        return tokensNoWhitespace;
+    };
+
+    function searchTokenPattern(pattern, tokens) {
+        let successful = 0;
+        let successfulTokens = [];
+        for (const token of tokens) {
+            if (pattern[successful].test(token)) {
+                successful++;
+                successfulTokens.push(token);
+                if (successful >= pattern.length) {
+                    return successfulTokens;
+                };
+            } else if (successful) {
+                successful = 0;
+                successfulTokens = [];
+            };
+        };
+        return null;
+    };
+
+    function searchFunction(tokens, name, namespace = null) {
+        const nameRegex = new RegExp('^' + name + '$');
+        if (searchTokenPattern([nameRegex], tokens)) {
+            return true;
+        } else if (namespace && searchTokenPattern([new RegExp('^' + namespace + '$'), /^::$/, nameRegex], tokens)) {
+            return true;
+        };
+    };
+
+    function constructTemplate(declaration) {
+        const providedTemplate = declaration.replace(/>\s*{$/, '').replace(/^class [a-zA-Z_][a-zA-Z\d_]*</, '');
+        const items = providedTemplate.split(/,\s*/);
+        const requires = [];
+        let output = 'template<';
+        for (let i in items) {
+            let isLast = i == items.length - 1;
+            let item = items[i].trim();
+            if (!item) continue;
+            if (!/^([a-zA-Z_][a-zA-Z\d_]*\s+)?[a-zA-Z_][a-zA-Z\d_]*(<[a-zA-Z\d_.-<>]*>)?(\s+requires\s+[a-zA-Z_<>][a-zA-Z\d_<>]*)?(\s*=\s*[a-zA-Z\d_.-<>][a-zA-Z\d_.-<>]*)?$/.test(item)) {
+                throw new SyntaxError(`Invalid syntax for template: ${item}`);
+            };
+            let requiresClause = item.match(/(?<=\s+requires\s+)[a-zA-Z_<>][a-zA-Z\d_<>]*(?=(\s*=\s*[a-zA-Z\d_.-<>][a-zA-Z\d_.-<>]*)?$)/)?.[0];
+            if (requiresClause) requires.push(requiresClause);
+    
+            let isClass = /^class\s+[a-zA-Z_][a-zA-Z\d_]*/.test(item);
+            let isTemplateClass = isClass && />$/.test(item);
+            if (isTemplateClass) {
+                let template = item.match(/(?<=^class\s+[a-zA-Z_][a-zA-Z\d_]*)<[a-zA-Z\d_.-<>]*>$/)?.[0];
+                if (!template) throw new SyntaxError(`Invalid syntax for template: ${item}`);
+                let decl = item.replace(template, '');
+                output += `template${template} ${decl}`;
+            } else if (isClass) {
+                output += item;
+            } else {
+                let isTypename = /^[a-zA-Z_][a-zA-Z\d_]*(<[a-zA-Z\d_.-<>]*>)?(\s+requires\s+[a-zA-Z_<>][a-zA-Z\d_<>]*)?(\s*=\s*[a-zA-Z\d_.-<>][a-zA-Z\d_.-<>]*)?$/.test(item);
+                if (isTypename) output += 'typename ';
+                let defaultValue = item.match(/(?<=\s*=\s*)[a-zA-Z\d_.-<>][a-zA-Z\d_.-<>]*$/)?.[0];
+                let name = isTypename ? item.match(/^[a-zA-Z_][a-zA-Z\d_]*/)?.[0] : item.match(/^[a-zA-Z_][a-zA-Z\d_]*\s+[a-zA-Z_][a-zA-Z\d_]*/)?.[0];
+                if (!name) throw new SyntaxError(`Invalid syntax for template: ${item}`);
+                output += name;
+                if (defaultValue) output += ' = ' + defaultValue;
+            };
+            if (!isLast) output += ', ';
+        };
+        output += '>\n';
+        if (requires.length > 0) {
+            output += `requires ${requires.join(' && ')}\n`;
+        };
+        output += declaration.replace('<' + providedTemplate + '>', '');
+        return output;
     };
 
     const trimmed = filePath.trim();
@@ -320,18 +512,27 @@ function compile(filePath) {
 
     let namespaceCode = `namespace c6 {\n`;
 
-    let mapped = c6Code;
+    const tokens = getTokens(c6Code);
     for (let func in customFunctionMap) {
-        if (new RegExp('\\b' + func + '\\b').test(mapped)) {
+        const matched = searchFunction(tokens, func, 'c6');
+        if (matched) {
             namespaceCode += constructCPPFunction(func);
         };
     };
 
     namespaceCode += '\n};\n';
-    mapped = namespaceCode + mapped;
+    c6Code = namespaceCode + c6Code;
 
-    const requiredHeaders = Array.from(getRequiredHeaders(mapped));
-    const output = requiredHeaders.map(header => `#include ${header}`).join('\n') + '\n' + mapped;
+    const requiredHeaders = Array.from(getRequiredHeaders(getTokens(c6Code)));
+    const headersOutput = requiredHeaders.map(header => `#include ${header}`).join('\n') + '\n' + c6Code;
+
+    const templates = headersOutput.replace(/\bclass [a-zA-Z\d_]+\s*<[^\n]*>\s*{/g, t => {
+        return constructTemplate(t);
+    });
+
+    const lambdas = templates.replace(/\bfn(?=\()/, '[&]');
+
+    const output = lambdas;
 
     const fileName = path.basename(trimmed);
     const dirName = path.dirname(trimmed);
@@ -341,40 +542,63 @@ function compile(filePath) {
 
 if (args.length == 0) {
     console.log(`
-C6 COMPILER HELP - VERSION 1.1.0
+C6 COMPILER HELP - VERSION 1.2.0
 
-c6 [filepath] - Compiles the file at [filepath] to a C++ file of the same name in the same directory.
-\x1b[30mNote: Compiler does not have an error checker. If the C6 code has an error, it may still compile.\x1b[0m
-c6 \x1b[30m--docs\x1b[0m - Will log the documentations for C6.
+c6 <filepath> - Compiles the file at <filepath> to a C++ file of the same name in the same directory.
+\x1b[38;5;240mNote: Compiler does not have an error checker. If the C6 code has an error, it may still compile.\x1b[0m
+c6 [\x1b[38;5;240m-d\x1b[0m|\x1b[38;5;240m--docs\x1b[0m] - Will log the documentations for the functions that are provided under the C6 namespace.
+c6 [\x1b[38;5;240m-v\x1b[0m|\x1b[38;5;240m--version\x1b[0m] - Will log the current version of C6.
+c6 \x1b[38;5;240m--benchmark\x1b[0m <filepath> - Will compile the file at <filepath> and log how long it took to compile.
     `);
     process.exit(0);
 } else {
-    if (args[0] == '--docs') {
+    if (args[0] == '--docs' || args[0] == '-d') {
         console.log(`
-\x1b[1mint c6::randint(int min, int max)\x1b[0m
+\x1b[1mfloat c6::randfloat(const float min, const float max)\x1b[0m
+Generates a random float between the min and the max values then returns it.
+
+\x1b[1mint c6::randint(const int min, const int max)\x1b[0m
 Generates a random integer between the min and the max values then returns it.
 
-\x1b[1mvoid c6::write_file(std::string path, std::string contents, bool overwrite = true)\x1b[0m
+\x1b[1mvoid c6::write_file(const std::string& path, const std::string& contents, const bool overwrite = true)\x1b[0m
 Writes the contents to the file at the provided path. Overwrite is optional and determines whether to overwrite
 the file or append to it. If there is no file at the provided path it will raise a std::runtime_error.
 
-\x1b[1mstd::string c6::read_file(std::string path)\x1b[0m
+\x1b[1mstd::string c6::read_file(const std::string& path)\x1b[0m
 Gets the contents of the file at the provided path and returns them. If there is no file at the provided
 path it will raise a std::runtime_error.
 
-\x1b[1mvoid c6::println(T text)\x1b[0m
+\x1b[1mvoid c6::println(const T... text)\x1b[0m
 Prints the provided text to the console with an automatic newline. The text can be a variety of types, including
 std::string, int, long, double, float, and other primitive types.
 
-\x1b[1mstd::string c6::input(T text)\x1b[0m
+\x1b[1mstd::string c6::input(const T& text)\x1b[0m
 Prints the provided text to the console and waits for user input. Once it recieves user input, it will return it as a std::string.
 
-\x1b[1mint c6::to_int(std::string value)\x1b[0m
+\x1b[1mint c6::to_int(const std::string& value)\x1b[0m
 Attempts to convert the std::string value to an int then return it. If it fails, it will throw a std::runtime_error.
 
-\x1b[1mstd::string c6::to_string(T value)\x1b[0m
+\x1b[1mstd::string c6::to_string(const T value)\x1b[0m
 Will convert any primitive type to a string (int, float, double, long, bool, etc).
+
+Please review the GitHub page for details on other aspects of C6!
         `);
+        process.exit(0);
+    };
+    if (args[0] == '--version' || args[0] == '-v') {
+        console.log('C6 Version 1.2.0');
+        process.exit(0);
+    };
+    if (args[0] == '--benchmark') {
+        if (!args[1]) {
+            console.log('You must specify a file path!');
+            process.exit(1);
+        };
+        const start = performance.now();
+        console.log('Compiling C6...');
+        compile(args[1]);
+        console.log('Compilation successful!');
+        console.log('[BENCHMARK] Time took: ' + (Math.round((performance.now() - start) * 10000) / 10000) + 'ms!');
         process.exit(0);
     };
     console.log('Compiling C6...');
